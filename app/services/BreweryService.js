1e71186f9f8f900642d2377d248c24ece123b607
Zachary.Brooks@flyfrontier.com <Zachary.Brooks@flyfrontier.com>
2018-10-29 16:33:43 -0600

    Add/Remove BreweryService Functions
    Added new functions for added breweries and beers to firebase realdb for a user. Add  vue-uuid for random if generation

---
+++ app/services/BreweryService.js
@@ -0,0 +1,120 @@
+import firebase from 'nativescript-plugin-firebase'
+import store from '../store';
+import uuid from 'vue-uuid';
+
+export default class BreweryService {
+    getCurrentUser() {
+        firebase.getCurrentUser().then(
+          function (user) {
+            console.log("getting user")
+            var path = "/Users/" + user.uid + "/Breweries";
+            var onValueEvent = function (result) {
+              if (result.error) {
+                console.log(result.error)
+              } else {
+                if (result.value == null) {
+                  console.log("no breweries!");
+                } else {
+                  const obj = result.value;
+                  const breweries = Object.keys(obj || {}).map(key => ({
+                    id: key,
+                    BreweryName: obj[key].BreweryName
+                  }));
+                  console.log(breweries);
+                  store.commit('setBreweries', breweries);
+                }
+              }
+            };
+    
+            firebase.addValueEventListener(onValueEvent, path).then(
+              function (result) {
+                that._listenerWrapper = result;
+                console.log("firebase.addValueEventListener added");
+              },
+              function (error) {
+                console.log("firebase.addValueEventListener error: " + error);
+              }
+            );
+          }
+        );
+    }
+
+    addBrewery(brewery) {
+        const path = "/Users/" + store.user.uid + "/Breweries";
+
+        firebase.push(
+            path,
+            {
+                'uid': uuid.v4(),
+                'name': brewery.name,
+                'address': brewery.address,
+                'coordinates': {
+                    'lng': brewery.coords.lng, 
+                    'lat': brewery.coords.lat
+                },
+                'beers': brewery.beers
+            }
+        ).then(
+            function(result) {
+                store.commit("pushBrewery", brewery);
+                console.log("New Brewery Added: " + result.key);
+            },
+            error => {
+              console.log("firebase.push error: " + error);
+            }
+        );
+    }
+
+    removeBrewery(brewery) {
+        const path = "/Users/" + store.user.uid + "/Breweries/" + brewery.uid;
+
+        firebase.remove(path)
+        .then(
+            function(result) {
+                console.log("Removed Brewery: " + result.key);
+            },
+            error => {
+                console.log("firebase.remove error: " + error);
+            }
+        )
+    }
+
+    addBeer(brewery, beer) {
+        const path = "/Users/" + store.user.uid + "/Breweries/" + brewery.uid;
+
+        firebase.push(
+            path,
+            {
+                'uid': uuid.v4(),
+                'name': beer.name,
+                'tyep': beer.type,
+                'ibu': beer.ibu,
+                'alcohol': beer.alcohol,
+                'rating': beer.rating,
+                'image': beer.image
+            }
+        ).then(
+            function(result) {
+                console.log("New Beer Added: " + result.key);
+                store.commit('pushBeer', brewery, beer);
+            },
+            error => {
+              console.log("firebase.push error: " + error);
+            }
+        );
+    }
+
+    removeBeer(brewery, beer) {
+        const path = "/Users/" + store.user.uid + "/Breweries/" + brewery.uid + "/Beers/" + beer.uid; 
+
+        firebase.remove(path)
+        .then(
+            function(result) {
+                console.log("Removed Beer: " + result.key);
+            },
+            error => {
+                console.log("firebase.remove error: " + error);
+            }
+        )
+    }
+}
--- app/store/index.js
+++ app/store/index.js
@@ -1,24 +1,97 @@
 import Vue from 'nativescript-vue';
 import Vuex from 'vuex';
 import firebase from 'nativescript-plugin-firebase';
+import BreweryService from '../services/BreweryService';
 
 Vue.use(Vuex);
 
 const state = {
-    user: null
+    user: null,
+    message: null,
+    info: null,
+    breweries: [],
+    selectedBrewery: null,
+    selectedBeer: null
 }
 
 const getters = {
-
+    brewery: state => {
+        return state.breweries.find(brewery => brewery.uid === state.selectedBrewery);
+    },
+    // beers: state => {
+    //     return state.breweries.find(brewery => brewery.beers)
+    // },
+    beer: state => {
+        return state.breweries.find(brewery => brewery.beers.find(beer => beer.uid === state.selectedBeer));
+    }
 }
 
 const mutations = {
     setUser: (state, user) => {
         state.user = user;
+    },
+    setSelectedBrewery: (state, id) => {
+        state.selectedBrewery = id;
+    },
+    setSelectedBeer: (state, id) => {
+        state.selectedBeer = id;
+    },
+    setBreweries: (state, breweries) => {
+        state.breweries = breweries;
+    },
+    pushBrewery: (state, brewery) => {
+        state.breweries.push(brewery);
+    },
+    pushBeer: (state, brewery, beer) => {
+        state.breweries[brewery].beers.push(beer);
+    },
+    setMessage: (state, message) => {
+        state.message = message;
+    },
+    setInfo: (state, info) => {
+        state.info = info;
+        //timeout the view
+        setTimeout(function () {
+        state.info = '';
+        }, 2000);
     }
 }
 
 const actions = {
+    fetchBreweries() {
+        BreweryService.getCurrentUser();
+    },
+
+    updateUser({commit}, user) {
+        firebase.update(
+          '/Users/' + user.uid,
+          {
+            DisplayName: user.email,
+            Email: user.name,
+            id: user.uid,
+            updateTs: firebase.ServerValue.TIMESTAMP
+          }).then(
+          () => {
+            //commit('setUser', payload);
+          },
+          error => {
+            console.log("firebase.setValue error: " + error);
+          }
+        );
+    },
+
+    addBrewery(brewery) {
+        BreweryService.addBrewery(brewery);
+    },
+
+    removeBrewery(brewery) {
+        BreweryService.removeBrewery(brewery);
+    },
+
+    addBeer(brewery, beer) {
+        BreweryService.addBeer(brewery, beer);
+    },
+
     fetchCurrentUser({commit}) {
         firebase.getCurrentUser().then(
           function (user) {
